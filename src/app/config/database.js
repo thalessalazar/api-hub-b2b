e6a2b7de745e208ab4ab1ec50ab4923a3d6bd68d
Thales Salazar <thalessalazar.12@gmail.com>
2021-06-01 18:31:34 -0300

    Correção do repo

---
+++ src/app/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: process.env.DATABASE_DIALECT,
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_BASE,
+    define: {
+        timeStamp: true, // createdAt - updatedAt
+        underscored: true, // underline format - not camelCase
+        underscoredAll: true,
+    },
+};
